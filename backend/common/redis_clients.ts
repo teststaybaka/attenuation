68f5a66fabe4e20618b03354be8befb9e29bc1cf
Yuxuan Xie <teststaybaka@gmail.com>
2022-05-03 06:31:37 +0000

    Format

--- backend/common/redis_clients.ts
+++ backend/common/redis_clients.ts
@@ -5,7 +5,9 @@ import { LOGGER } from "./logger";
 export let REDIS_CLIENTS = new Array<[string, redis.RedisClientType]>();
 
 // URL format is `${host}:${port}`
-export async function createRedisClients(...urls: Array<string>): Promise<void> {
+export async function createRedisClients(
+  ...urls: Array<string>
+): Promise<void> {
   for (let url of urls) {
     let client: redis.RedisClientType = redis.createClient({
       url: `redis://${url}`,
