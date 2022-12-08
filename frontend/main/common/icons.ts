fc5a8981b08c9d0ea550419fdb6984857b4d2784
Yuxuan Xie <teststaybaka@gmail.com>
2022-12-08 06:34:58 +0000

    Add fullscreen ImageViewer.

--- frontend/main/common/icons.ts
+++ frontend/main/common/icons.ts
@@ -9,7 +9,21 @@ export function createPlusIcon(color: string): SVGSVGElement {
       fill: color,
     },
     E.path({
-      d: `M25 80 h150 v40 h-150 z M80 25 h40 v150 h-40 z`,
+      d: `M25 85 h150 v30 h-150 z  M85 25 h30 v150 h-30 z`,
+    })
+  );
+}
+
+export function createMinusIcon(color: string): SVGSVGElement {
+  return E.svg(
+    {
+      class: "plus-icon",
+      style: `height: 100%;`,
+      viewBox: "0 0 200 200",
+      fill: color,
+    },
+    E.path({
+      d: `M25 85 h150 v30 h-150 z`,
     })
   );
 }
@@ -51,6 +65,20 @@ export function createTrashCanIcon(color: string): SVGSVGElement {
     },
     E.path({
       d: `M 512,800 V 224 q 0,-14 -9,-23 -9,-9 -23,-9 h -64 q -14,0 -23,9 -9,9 -9,23 v 576 q 0,14 9,23 9,9 23,9 h 64 q 14,0 23,-9 9,-9 9,-23 z m 256,0 V 224 q 0,-14 -9,-23 -9,-9 -23,-9 h -64 q -14,0 -23,9 -9,9 -9,23 v 576 q 0,14 9,23 9,9 23,9 h 64 q 14,0 23,-9 9,-9 9,-23 z m 256,0 V 224 q 0,-14 -9,-23 -9,-9 -23,-9 h -64 q -14,0 -23,9 -9,9 -9,23 v 576 q 0,14 9,23 9,9 23,9 h 64 q 14,0 23,-9 9,-9 9,-23 z M 1152,76 v 948 H 256 V 76 Q 256,54 263,35.5 270,17 277.5,8.5 285,0 288,0 h 832 q 3,0 10.5,8.5 7.5,8.5 14.5,27 7,18.5 7,40.5 z M 480,1152 h 448 l -48,117 q -7,9 -17,11 H 546 q -10,-2 -17,-11 z m 928,-32 v -64 q 0,-14 -9,-23 -9,-9 -23,-9 h -96 V 76 q 0,-83 -47,-143.5 -47,-60.5 -113,-60.5 H 288 q -66,0 -113,58.5 Q 128,-11 128,72 v 952 H 32 q -14,0 -23,9 -9,9 -9,23 v 64 q 0,14 9,23 9,9 23,9 h 309 l 70,167 q 15,37 54,63 39,26 79,26 h 320 q 40,0 79,-26 39,-26 54,-63 l 70,-167 h 309 q 14,0 23,-9 9,-9 9,-23 z`,
+    })
+  );
+}
+
+export function createExpandIcon(color: string): SVGSVGElement {
+  return E.svg(
+    {
+      class: "plus-icon",
+      style: `height: 100%;`,
+      viewBox: "0 0 200 200",
+      fill: color,
+    },
+    E.path({
+      d: `M20 180 h70 v-20 h-36 L93 120 L80 106 L40 146 v-36 h-20 z  M180 20 h-70 v20 h36 L106 80 L120 93 L160 54 v36 h20 z`,
     })
   );
 }
---
+++ frontend/main/content_page/post_entry_list_page/common/image_viewer.ts
@@ -0,0 +1,125 @@
+import EventEmitter = require("events");
+import { SCHEME } from "../../../common/color_scheme";
+import {
+  createExpandIcon,
+  createMinusIcon,
+  createPlusIcon,
+} from "../../../common/icons";
+import { E } from "@selfage/element/factory";
+import { Ref } from "@selfage/ref";
+
+export interface ImageViewer {
+  on(event: "load", listener: () => void): this;
+}
+
+export class ImageViewer extends EventEmitter {
+  private static BUTTON_STYLE = `width: 3rem; height: 3rem; border-radius: .3rem; background-color: ${SCHEME.neutral4}; color: ${SCHEME.neutral1};`;
+
+  public body: HTMLDivElement;
+  public controllerBodies = new Array<HTMLDivElement>();
+  private zoomInButton: HTMLDivElement;
+  private zommOutButton: HTMLDivElement;
+  private zommFitButton: HTMLDivElement;
+  private image: HTMLImageElement;
+
+  public constructor(imageUrl: string, display: boolean) {
+    super();
+    this.zoomInButton = E.div(
+      {
+        class: "image-viewer-zoom-in-button",
+        style: ImageViewer.BUTTON_STYLE,
+      },
+      createPlusIcon("currentColor")
+    );
+    this.zommOutButton = E.div(
+      {
+        class: "image-viewer-zoom-out-button",
+        style: ImageViewer.BUTTON_STYLE,
+      },
+      createMinusIcon("currentColor")
+    );
+    this.zommFitButton = E.div(
+      {
+        class: "image-viewer-zoom-fit-button",
+        style: ImageViewer.BUTTON_STYLE,
+      },
+      createExpandIcon("currentColor")
+    );
+    this.controllerBodies.push(
+      this.zommFitButton,
+      this.zommOutButton,
+      this.zoomInButton
+    );
+    let imageRef = new Ref<HTMLImageElement>();
+    this.body = E.div(
+      {
+        class: "image-viewer-image-scroller",
+        style: `flex-flow: column nowrap; min-width: 100vw; min-height: 100vh;`,
+      },
+      E.imageRef(imageRef, {
+        class: "image-viewer-image",
+        style: `flex: 0 0 auto; margin: auto;`,
+      })
+    );
+    this.image = imageRef.val;
+    if (display) {
+      this.show();
+    } else {
+      this.hide();
+    }
+
+    this.zoomInButton.addEventListener("click", () => this.zoomInImage());
+    this.zommOutButton.addEventListener("click", () => this.zoomOutImage());
+    this.zommFitButton.addEventListener("click", () => this.fitImage());
+    this.image.onload = () => this.load();
+    this.image.src = imageUrl;
+  }
+
+  private zoomInImage(): void {
+    this.zoomImage(1.2);
+  }
+
+  private zoomOutImage(): void {
+    this.zoomImage(0.8);
+  }
+
+  private zoomImage(percentage: number): void {
+    let clientWidth = this.image.clientWidth;
+    let newClientWidth = clientWidth * percentage;
+    let newScrollX = window.scrollX + newClientWidth / 2 - clientWidth / 2;
+    let clientHeight = this.image.clientHeight;
+    let newClientHeight = clientHeight * percentage;
+    let newScrollY = window.scrollY + newClientHeight / 2 - clientHeight / 2;
+    this.image.style.width = `${newClientWidth}px`;
+    this.image.clientWidth; // Force flush
+    window.scrollTo(newScrollX, newScrollY);
+  }
+
+  private fitImage(): void {
+    let documentWidth = document.documentElement.clientWidth;
+    let documentHeight = document.documentElement.clientHeight;
+    if (
+      this.image.naturalWidth / this.image.naturalHeight >
+      documentWidth / documentHeight
+    ) {
+      this.image.style.width = `${documentWidth}px`;
+    } else {
+      this.image.style.width = `${
+        (this.image.naturalWidth / this.image.naturalHeight) * documentHeight
+      }px`;
+    }
+  }
+
+  private load(): void {
+    this.fitImage();
+    this.emit("load");
+  }
+
+  public show(): void {
+    this.body.style.display = "flex";
+  }
+
+  public hide(): void {
+    this.body.style.display = "none";
+  }
+}
